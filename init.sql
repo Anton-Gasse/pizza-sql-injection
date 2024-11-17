7344c4557f16ea13a1ce89cba201dc0e428b908d
Jakob Elster <elster.jkb@gmail.com>
2024-11-17 18:44:17 +0100

    Add backend user with read only access to Coupon table

--- init.sql
+++ init.sql
@@ -8,3 +8,9 @@ CREATE TABLE IF NOT EXISTS Coupon (
 -- insert coupons
 INSERT INTO Coupon(code, description) VALUES ('test100', 'Coupon Code for test purposes. Gives 100% off.');
 INSERT INTO Coupon(code, description) VALUES ('test50', 'Coupon Code for test purposes. Gives 50% off.');
+
+-- create test user for back-end
+CREATE USER backend WITH 'password';
+GRANT CONNECT ON DATABASE postgres TO backend;
+GRANT USAGE ON SCHEMA public TO backend;
+GRANT SELECT ON Coupon TO backend;
